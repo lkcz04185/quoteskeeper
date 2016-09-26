3a0eeb6e8d578a02e8910eac90bfe0c7fbd83703
lkcz04185 <lkcz04185@gmail.com>
2016-09-26 20:56:19 +0000

    input error message

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+  validates :saying, presence: true, length: {maximum: 140, minimum: 10}
+  validates :author, presence: true, length: {maximum: 50, minimum: 6}
 end
