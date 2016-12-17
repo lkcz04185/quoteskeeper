3fd51e1f83cb616c6ac0200470c354483f46c9a3
lkcz04185 <lkcz04185@gmail.com>
2016-12-17 21:13:36 +0000

    edit action

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -13,13 +13,26 @@ class QuotesController < ApplicationController
   def create
     @quote = Quote.create(quote_params)
     if @quote.invalid?
-      flash[:error] = '<strong> Could not save</strong> the data you entered is invalid.'
+      flash[:error] = "<strong>Could not save</strong> #{@quote.errors.full_messages.join(", ")}."
     end
     redirect_to root_path
   end
 
-  def about
+  def edit
+    @quote = Quote.find(params[:id])
+  end
 
+  def update
+    @quote = Quote.find(params[:id])
+
+    if @quote.update_attributes(quote_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def about
   end
 
   private
