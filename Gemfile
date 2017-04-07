56f6c14f0f2908420a0a7fca377b496a3790995d
lkcz04185 <lkcz04185@gmail.com>
2017-04-07 15:57:55 +0000

    rename

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'rename'
 ruby '2.4.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_splurty_lk_session'
+Rails.application.config.session_store :cookie_store, key: '_quotesgen_session'
